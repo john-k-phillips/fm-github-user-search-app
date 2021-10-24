b255f7a1fc755b31e0f61fa2f722c7c05105c081
John Phillips <johnphillips.dev@gmail.com>
2021-10-24 21:29:46 +0100

    Passes information into userdata function.

--- src/dataRequest.js
+++ src/dataRequest.js
@@ -1,9 +1,10 @@
-const sendRequest = (username) => {
+const sendRequest = (username, setUserData) => {
   const apiUrl = `https://api.github.com/users/${username}`;
   fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
+      setUserData(data);
     });
 };
 
