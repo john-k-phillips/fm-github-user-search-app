5374774d12352b69877cb2df311fe75e069d12ec
John Phillips <johnphillips.dev@gmail.com>
2021-10-24 21:28:57 +0100

    Card displays when user information is found.

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import React, { useState, useEffect } from 'react';
 
 import Navbar from './components/header/Navbar';
 import SearchBar from './components/header/SearchBar';
+import ProfileCard from './components/main/ProfileCard';
 
 import requestData from './dataRequest';
 
 export default function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
   const [userInput, setUserInput] = useState('');
+  const [userData, setUserData] = useState({});
 
   useEffect(() => {
-    requestData(userInput);
+    requestData(userInput, setUserData);
   }, [userInput]);
 
   document.body.style.backgroundColor = isDarkMode ? '#141D2F' : '#F6F8FF';
@@ -19,6 +21,7 @@ export default function App() {
     <div className={isDarkMode ? 'dark' : 'light'}>
       <Navbar isDarkMode={isDarkMode} setTheme={setIsDarkMode} />
       <SearchBar isDarkMode={isDarkMode} setUserInput={setUserInput} />
+      {userData.message === 'Not Found' ? '' : <ProfileCard userData={userData} />}
     </div>
   );
 }
