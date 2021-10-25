a114c3c428e2990d50b090acb7575d2141f91980
John Phillips <johnphillips.dev@gmail.com>
2021-10-25 12:07:14 +0100

    Profile stats now display correctly.

--- src/components/main/ProfileStats.jsx
+++ src/components/main/ProfileStats.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import StatsCard from './StatsCard';
 
+import InfoComponent from './InfoComponent';
+
+import { ReactComponent as LocationIcon } from '../../assets/icon-location.svg';
+import { ReactComponent as WebsiteIcon } from '../../assets/icon-website.svg';
+import { ReactComponent as TwitterIcon } from '../../assets/icon-twitter.svg';
+import { ReactComponent as CompanyIcon } from '../../assets/icon-company.svg';
+
 export default function ProfileStats({
-  bio, repos, followers, following,
+  bio, repos, followers, following, location, blog, twitter, company, isDarkMode,
 }) {
   return (
     <div>
@@ -24,8 +32,13 @@ export default function ProfileStats({
         <StatsCard title="Followers" followers={followers} />
         <StatsCard title="Following" following={following} />
       </div>
-      <div>
-        {/* Extra Info */}
+      <div
+        className="flex flex-col gap-2 mt-5 pb-5"
+      >
+        <InfoComponent image={<LocationIcon fill={isDarkMode ? 'white' : '#4b6a9b'} />} info={location} extraPadding />
+        <InfoComponent image={<WebsiteIcon fill={isDarkMode ? 'white' : '#4b6a9b'} />} info={blog} />
+        <InfoComponent image={<TwitterIcon fill={isDarkMode ? 'white' : '#4b6a9b'} />} info={twitter} />
+        <InfoComponent image={<CompanyIcon fill={isDarkMode ? 'white' : '#4b6a9b'} />} info={company} />
       </div>
     </div>
   );
@@ -33,13 +46,27 @@ export default function ProfileStats({
 
 ProfileStats.propTypes = {
   bio: PropTypes.string,
+
+  location: PropTypes.string,
+  blog: PropTypes.string,
+  twitter: PropTypes.string,
+  company: PropTypes.string,
+
   repos: PropTypes.number,
   followers: PropTypes.number,
   following: PropTypes.number,
+
+  isDarkMode: PropTypes.bool.isRequired,
 };
 
 ProfileStats.defaultProps = {
   bio: '',
+
+  location: 'Not Available',
+  blog: 'Not Available',
+  twitter: 'Not Available',
+  company: 'Not Available',
+
   repos: 0,
   followers: 0,
   following: 0,
