73905fa3c4b75e73b90a3c389e3fdb1304fa92ec
John Phillips <johnphillips.dev@gmail.com>
2021-10-25 11:08:45 +0100

    Bio added with repo card.

---
+++ src/components/main/StatsCard.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+export default function StatsCard({
+  repos, followers, following, title,
+}) {
+  let valueElement;
+  switch (title) {
+    case 'Repos':
+      valueElement = repos;
+      break;
+    case 'Following':
+      valueElement = following;
+      break;
+    case 'Followers':
+      valueElement = followers;
+      break;
+    default:
+      valueElement = 0;
+  }
+  return (
+    <div className="flex flex-col justify-center text-center text-xs gap-1">
+      <p>{title}</p>
+      <span
+        className="
+        font-bold text-xl
+        dark:text-custom-grayish-white-lighter"
+      >
+        {valueElement}
+
+      </span>
+    </div>
+  );
+}
+
+StatsCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  repos: PropTypes.number,
+  followers: PropTypes.number,
+  following: PropTypes.number,
+};
+
+StatsCard.defaultProps = {
+  repos: 0,
+  followers: 0,
+  following: 0,
+};
