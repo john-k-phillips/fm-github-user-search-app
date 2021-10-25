aaab0aa15237462a3f712c279495730eb9cdd390
John Phillips <johnphillips.dev@gmail.com>
2021-10-25 14:26:18 +0100

    Responsive for mobile and desktop.

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -10,14 +10,19 @@ export default function Navbar({ setTheme, isDarkMode }) {
   };
 
   return (
-    <nav className="w-10/12 flex m-auto mt-10 items-center">
+    <nav
+      className="
+      w-10/12 flex m-auto mt-10 items-center
+      md:mt-6
+      lg:w-full"
+    >
       <span
         className="
         mr-auto text-2xl font-bold text-custom-black-lighter
-        dark:text-white"
+        dark:text-white
+        md:text-3xl"
       >
         devfinder
-
       </span>
       <div
         className="flex items-center gap-3 cursor-pointer"
@@ -29,7 +34,8 @@ export default function Navbar({ setTheme, isDarkMode }) {
         <h2
           className="
           font-bold text-github-blue text-custom-grayish-blue-lighter tracking-widest
-          dark:text-white"
+          dark:text-white
+          md:text-xl"
         >
           {isDarkMode ? 'Light' : 'Dark'}
         </h2>
